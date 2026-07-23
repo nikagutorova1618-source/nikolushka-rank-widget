99f9547359148000c2959514d6e8383182f74022
nikagutorova1618-source <nika.gutorova1618@gmail.com>
2026-07-23 20:50:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-// Пока это тестовые данные.
-// Позже мы подключим автоматическое обновление.
-
-const rankElement = document.getElementById("rank");
-const rrElement = document.getElementById("rr");
-
-const player = {
-    rank: "Ascendant 2",
-    rr: 67
-};
-
-rankElement.textContent = player.rank;
-rrElement.textContent = player.rr + " RR";
+// Пока пусто.
+// Позже здесь будет автоматическое обновление ранга.
